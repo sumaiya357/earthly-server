22f414e9ffc82f6b7071662d51c3d79f3e958c6a
sumaiya357 <rahmansumaiya75@gmail>
2023-03-14 02:05:42 +0600

    all category , admin, user panel ready

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ async function run() {
       const fruitsCollection = client.db('earthlyDelights').collection('fruits');
       const indoorsCollection = client.db('earthlyDelights').collection('indoors');
       const bonsaiCollection = client.db('earthlyDelights').collection('bonsai');
+      const spicesCollection = client.db('earthlyDelights').collection('spices');
+      const toolsCollection = client.db('earthlyDelights').collection('tools');
       const addedProductsCollection = client.db('earthlyDelights').collection('addedProducts');
 
       const usersCollection = client.db('earthlyDelights').collection('users');
@@ -77,6 +79,16 @@ async function run() {
          const allBonsai = await bonsaiCollection.find(query).toArray();
          res.send(allBonsai);
       })
+      app.get('/spices', async (req, res) => {
+         const query = {};
+         const spices = await spicesCollection.find(query).toArray();
+         res.send(spices);
+      })
+      app.get('/tools', async (req, res) => {
+         const query = {};
+         const tools = await toolsCollection.find(query).toArray();
+         res.send(tools);
+      })
 
       //SEND THE PRODUCTS TO BACKEND BY ADMIN
       app.post('/addedProducts', async (req, res) => {
@@ -108,17 +120,19 @@ async function run() {
       })
 
       //GET ALL USER FROM DB
-      app.get('/users', verifyJWT,  async (req, res) => {
-         const email = req.query.email;
-         const decodedEmail = req.decoded.email;
-         if(email !== decodedEmail){
-            return res.status(403).send({message: 'forbiden access'})
-         }
-         const query = req.body;
-         const allUsers = await usersCollection.find(query).toArray();
-         res.send(allUsers)
-      })
-      //GET ALL USER FROM DB
+      // app.get('/users', verifyJWT,  async (req, res) => {
+      //    const email = req.query.email;
+      //    const decodedEmail = req.decoded.email;
+      //    if(email !== decodedEmail){
+      //       return res.status(403).send({message: 'forbiden access'})
+      //    }
+      //    const query = req.body;
+      //    const allUsers = await usersCollection.find(query).toArray();
+      //    res.send(allUsers)
+      // })
+
+      
+      //------------- //GET ALL USER FROM DB //----------------
       //   app.get('/users', async(req,res) =>{
       //     const email = req.query.email;
       //     console.log(email)
@@ -126,38 +140,140 @@ async function run() {
       //     const allUser = await usersCollection.find(query).toArray();
       //     res.send(allUser)
       //  })
+        app.get('/users', async(req,res) =>{
+         //  const email = req.query.email;
+         //  console.log(email)
+          const query = {};
+          const allUser = await usersCollection.find(query).toArray();
+          res.send(allUser)
+       })
 
-      app.get('/users/admin/:email', async (req, res) => {
-         const email = req.query.email;
-         console.log(email)
-         const query = { email };
-         const allUser = await usersCollection.findOne(query);
-         res.send({ isAsmin: user?.role === 'admin' })
-      })
+      // app.get('/users/admin/:email',verifyJWT, async (req, res) => {
+      //    const email = req.query.email;
+      //    console.log(email)
+      //    const query = { email };
+      //    const allUser = await usersCollection.findOne(query);
+      //    res.send({ isAsmin: user?.role === 'admin' })
+      // })
 
 
-      //TO DELETE PRODUCT
+      //------// TO DELETE PRODUCT //-------------
+     
+     
+     
       app.delete('/products/:id', async (req, res) => {
          const id = req.params.id;
-         console.log(id)
+         // console.log(id)
          const filter = { _id: new ObjectId(id) };
          const result = await productsCollection.deleteOne(filter);
          res.send(result);
       })
 
-      app.put('/users/admin/:id', async (req, res) => {
+
+      //------// TO DELETE FLOWER //-------------
+      app.delete('/flowers/:id', async (req, res) => {
+         const id = req.params.id;
+         // console.log(id)
+         const filter = { _id: new ObjectId(id) };
+         const result = await flowersCollection.deleteOne(filter);
+         res.send(result);
+      })
+
+      //------// TO DELETE FRUIT //-------------
+      app.delete('/fruits/:id', async (req, res) => {
+         const id = req.params.id;
+         // console.log(id)
+         const filter = { _id: new ObjectId(id) };
+         const result = await fruitsCollection.deleteOne(filter);
+         res.send(result);
+      })
+
+      //------// TO DELETE BONSAI //-------------
+      app.delete('/bonsai/:id', async (req, res) => {
+         const id = req.params.id;
+         // console.log(id)
+         const filter = { _id: new ObjectId(id) };
+         const result = await bonsaiCollection.deleteOne(filter);
+         res.send(result);
+      })
+
+      //------// TO DELETE INDOOR //-------------
+      app.delete('/indoors/:id', async (req, res) => {
+         const id = req.params.id;
+         // console.log(id)
+         const filter = { _id: new ObjectId(id) };
+         const result = await indoorsCollection.deleteOne(filter);
+         res.send(result);
+      })
+
+      //------// TO DELETE SPICES //-------------
+      app.delete('/spices/:id', async (req, res) => {
+         const id = req.params.id;
+         // console.log(id)
+         const filter = { _id: new ObjectId(id) };
+         const result = await spicesCollection.deleteOne(filter);
+         res.send(result);
+      })
+      // //------// TO DELETE SPICES //-------------
+      app.delete('/tools/:id', async (req, res) => {
+         const id = req.params.id;
+         // console.log(id)
+         const filter = { _id: new ObjectId(id) };
+         const result = await toolsCollection.deleteOne(filter);
+         res.send(result);
+      })
+
+      //----------// SET ADMIN IN DB------------
+
+      // app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+      //    const id = req.params.id;
+      //    const filter = { _id: new ObjectId(id) }
+      //    const options = { upsert: true };
+      //    const updatedDoc = {
+      //       $set: {
+      //          role: 'admin'
+
+      //       }
+      //    }
+      //    const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      //    res.send(result)
+      // })
+
+
+         app.put('/users/admin/:id',verifyJWT, async (req, res) => {
+         const decodedEmail = req.decoded.email;
+         const query = {email: decodedEmail};
+         const user = await usersCollection.findOne(query);
+
+         if(user.role !== 'admin'){
+            return res.status(403).send({message: 'forbiden access'})
+         }
+
          const id = req.params.id;
          const filter = { _id: new ObjectId(id) }
          const options = { upsert: true };
-         const updateDoc = {
+         const updatedDoc = {
             $set: {
                role: 'admin'
 
             }
          }
-         const result = await usersCollection.updateOne(filter, updateDoc, options);
+         const result = await usersCollection.updateOne(filter, updatedDoc, options);
          res.send(result)
       })
+
+
+
+
+      //------------// CHECK ADMIN IN DB //--------------
+      app.get('/users/admin/:email', async (req, res) => {
+         const email = req.params.email;
+         const query = { email }
+         const user = await usersCollection.findOne(query);
+         res.send({isAdmin: user?.role === 'admin'});
+         
+      })
+   
    }
    finally {
    }
